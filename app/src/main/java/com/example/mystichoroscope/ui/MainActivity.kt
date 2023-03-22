9ef3c065c6ab6747a76d07220115d2b973ea8090
pablo <pnunfe@gmail.com>
2023-03-22 22:04:41 +0000

    Initial commit

---
+++ app/src/main/java/com/example/mystichoroscope/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mystichoroscope.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mystichoroscope.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
