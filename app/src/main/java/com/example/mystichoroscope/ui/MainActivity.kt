77a2c8ea6d5d3725cc58b3e56b2f0b837e7d6316
pablo <pnunfe@gmail.com>
2023-03-22 23:01:39 +0000

    Navigation complete

--- app/src/main/java/com/example/mystichoroscope/ui/MainActivity.kt
+++ app/src/main/java/com/example/mystichoroscope/ui/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.mystichoroscope.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.mystichoroscope.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_controller_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
