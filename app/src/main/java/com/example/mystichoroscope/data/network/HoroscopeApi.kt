293ff9f6b280ae49bb0d9fbb14ccbdc84da63946
pablo <pnunfe@gmail.com>
2023-03-28 22:27:55 +0100

    Implemented Retrofit

---
+++ app/src/main/java/com/example/mystichoroscope/data/network/HoroscopeApi.kt
@@ -0,0 +1,16 @@
+package com.example.mystichoroscope.data.network
+
+import com.example.mystichoroscope.data.network.model.HoroscopeResponse
+import retrofit2.Response
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface HoroscopeApi {
+
+    @POST(".")
+    suspend fun getHoroscope(
+        @Query("sing") sing:String,
+        @Query("day") day:String
+    ):Response<HoroscopeResponse>
+
+}
